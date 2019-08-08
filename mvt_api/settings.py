c55ec3dc0a432213e606b4fb3446882ee47d928a
ravi thokal <ravi.t@sankeysolutions.com>
2019-08-08 12:20:22 +0530

    remove whitenoise

--- mvt_api/settings.py
+++ mvt_api/settings.py
@@ -135,5 +135,3 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
